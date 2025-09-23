ce81da24de7fc1cdc0c089aec1bed443f2602019
hhoow0093 <howarddai2005@gmail.com>
2025-09-23 23:19:39 +0700

    Adding a RecyclerView and Populating it

---
+++ app/src/main/java/eu/tutorials/lab_week_06/ImageLoader.kt
@@ -0,0 +1,6 @@
+package eu.tutorials.lab_week_06
+import android.widget.ImageView;
+
+interface ImageLoader {
+    fun loadImage(imageUrl: String, imageView: ImageView)
+}
---
+++ app/src/main/java/eu/tutorials/lab_week_06/model/CatBreed.kt
@@ -0,0 +1,5 @@
+package eu.tutorials.lab_week_06.model
+
+enum class CatBreed {
+    AmericanCurl, BalineseJavanese, ExoticShorthair
+}
